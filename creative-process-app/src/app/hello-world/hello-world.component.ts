e51a54360777865e9d417f2813b36d3e8826e72d
Ian Perfitt <iancp@protonmail.com>
2024-10-26 21:26:43 -0500

    Implement hello-world component to display random oblilque strategy after clicking a button.

--- creative-process-app/src/app/hello-world/hello-world.component.ts
+++ creative-process-app/src/app/hello-world/hello-world.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StrategyService } from '../strategy.service';
 
 @Component({
-  selector: 'app-hello-world',
+  selector: 'hello-world',
   templateUrl: './hello-world.component.html',
   styleUrl: './hello-world.component.scss'
 })
-export class HelloWorldComponent {
+export class HelloWorldComponent implements OnInit{
+
+  strategies : string[] = [];
+  currentStrategy : string = "";
+
+  constructor(private strategyService : StrategyService) {}
+
+  ngOnInit(): void {
+    this.strategies = this.strategyService.getStrategies();
+  }
+
+  getRandomIndex(min : number, max : number) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  generateStrategy() {
+    let index = this.getRandomIndex(0, this.strategies.length - 1);
+    this.currentStrategy =  this.strategies[index];
+  }
+
 
 }
